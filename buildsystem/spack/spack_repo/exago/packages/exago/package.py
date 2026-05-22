adf7bd71ec574910003ae6be13d8a80f6b81b8c3
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2026-05-22 14:51:22 -0400

    Update Ipopt and PETSc dependencies in Spack package (develop --> 2.0:).

--- buildsystem/spack/spack_repo/exago/packages/exago/package.py
+++ buildsystem/spack/spack_repo/exago/packages/exago/package.py
@@ -32,6 +32,7 @@ class Exago(CMakePackage, CudaPackage, ROCmPackage):
     version(
         "2.0.0",
         tag="v2.0.0",
+        commit="d80d9a00914c096121832c6bb778d83b0b40c3c9",
         submodules=submodules,
     )
     version(
@@ -218,12 +219,12 @@ class Exago(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("petsc@3.16", when="@1.3:1.4")
     depends_on("petsc@3.18:3.19", when="@1.5")
     depends_on("petsc@3.19:3.23", when="@1.6")
-    depends_on("petsc@3.24:", when="@develop")
+    depends_on("petsc@3.24:", when="@2.0:")
     depends_on("petsc~mpi", when="~mpi")
 
     # Ipopt versiondependency logic
     depends_on("ipopt@3.12", when="@:1.6")
-    depends_on("ipopt@3.14:", when="@develop")
+    depends_on("ipopt@3.14:", when="@2.0:")
 
     # cuda_arch and amdgpu_target dependency logic
     for arch in CudaPackage.cuda_arch_values:
