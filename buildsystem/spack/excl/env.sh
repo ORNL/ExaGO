d1e5d7dd581fd4ec79af62086ff49c24d9db481d
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2026-09-18 16:14:13 -0400

    Remove user-specific path on excl.

--- buildsystem/spack/excl/env.sh
+++ buildsystem/spack/excl/env.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-BASE=/home/97k/Codes/ExaGO/.spack
-
-export SPACK_INSTALL=$BASE/spack-install
+export SPACK_INSTALL=spack-install
 export SPACK_MODULES=modules
-export SPACK_CACHE=$BASE/spack-cache
-export SPACK_MIRROR=$BASE/spack-mirror
+export SPACK_CACHE=spack-cache
+export SPACK_MIRROR=spack-mirror
